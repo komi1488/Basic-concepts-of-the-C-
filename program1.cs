9e4ceee4ba36f8fab47eb8ef41884669a3084ceb
komi <pashaandreev66666@gmail.com>
2026-02-11 19:41:12 +0700

    Добавил решения для задач

---
+++ program1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program1 {
+  static int Main() {
+    
+  int a = 0;
+  int n = 0;
+  int index = 0;
+  int result = 1;
+
+  Console.Write("a = ");
+  a = int.Parse(Console.ReadLine());
+
+  Console.Write("n = ");
+  n = int.Parse(Console.ReadLine());
+
+  for (index = 0; index < n; index++) {
+    result = result * a;
+  }
+
+  Console.WriteLine(a + "^" + n + " = " + result);
+  return 0;
+  }
+}
---
+++ program2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program2 {
+  static int Main() {
+  Console.Write("Enter the number x (at least 3 digits): ");
+  string input = Console.ReadLine();
+
+  int resultNumber = 0;
+  char firstDigit = input[0];
+  char secondDigit = input[1];
+  string rest = input.Substring(2);
+
+  string withoutSecond = firstDigit + rest;
+
+  string resultStr = withoutSecond + secondDigit;
+
+  resultNumber = int.Parse(resultStr);
+
+  Console.WriteLine($"n = {resultNumber}");
+  return 0;
+  }
+}
