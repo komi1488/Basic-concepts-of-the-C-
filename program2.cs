449b5e31c45106df0e58b3809d0492e57493aff7
komi <pashaandreev66666@gmail.com>
2026-02-11 21:41:25 +0700

    исправил недочеты

--- program2.cs
+++ program2.cs
@@ -1,21 +1,25 @@
 ﻿using System;
+
 class Program2 {
   static int Main() {
-  Console.Write("Enter the number x (at least 3 digits): ");
-  string input = Console.ReadLine();
+    Console.Write("Enter the number x (at least 3 digits): ");
+    
+    string input;
+    string digitsFromThird;
+    int resultNumber = 0;
 
-  int resultNumber = 0;
-  char firstDigit = input[0];
-  char secondDigit = input[1];
-  string rest = input.Substring(2);
+    input = Console.ReadLine();
+    char firstDigit = input[0];
+    char secondDigit = input[1];
+    digitsFromThird = input.Substring(2);
+    
+    string withoutSecond = firstDigit + digitsFromThird;
 
-  string withoutSecond = firstDigit + rest;
+    string resultStr = withoutSecond + secondDigit;
 
-  string resultStr = withoutSecond + secondDigit;
+    resultNumber = int.Parse(resultStr);
 
-  resultNumber = int.Parse(resultStr);
-
-  Console.WriteLine($"n = {resultNumber}");
-  return 0;
-  }
+    Console.WriteLine($"n = {resultNumber}");
+    return 0;
+    }
 }
