ecf54699724eb3331d66d9786cbdec5712c4c559
komi <pashaandreev66666@gmail.com>
2026-02-11 21:20:47 +0700

    исправил недочеты

--- program1.cs
+++ program1.cs
@@ -1,23 +1,24 @@
 ﻿using System;
+
 class Program1 {
   static int Main() {
     
-  int a = 0;
-  int n = 0;
-  int index = 0;
-  int result = 1;
+    int baseNumber = 0;
+    int exponent = 0;
+    int index = 0;
+    int result = 1;
 
-  Console.Write("a = ");
-  a = int.Parse(Console.ReadLine());
+    Console.Write("a = ");
+    baseNumber = int.Parse(Console.ReadLine());
 
-  Console.Write("n = ");
-  n = int.Parse(Console.ReadLine());
+    Console.Write("n = ");
+    exponent = int.Parse(Console.ReadLine());
 
-  for (index = 0; index < n; index++) {
-    result = result * a;
-  }
+    for (index = 0; index < exponent; ++index) {
+      result = result * baseNumber;
+    }
 
-  Console.WriteLine(a + "^" + n + " = " + result);
-  return 0;
-  }
+    Console.WriteLine(baseNumber + "^" + exponent + " = " + result);
+    return 0;
+    }
 }
